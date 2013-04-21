0b54ec41923498d64e4da115a1b0ff74802b1566
Christina Chang <christina.chang@flatironschool.com>
2013-04-21 17:33:30 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Statusbot::Application.config.secret_token = '3ce6496ae04bf22828f4c20f79378b953a301f1e82b72aa78fc4d045e6d67565d6360952af8c1008817a64bc74658c5a7de0dc9acfeec50505fa33f3c5f22f59'
