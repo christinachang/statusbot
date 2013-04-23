bcebd60f35f8d999ee6134232a7faaea1eb4c939
Christina Chang <christina.chang@flatironschool.com>
2013-04-23 17:15:31 -0400

    add scope to omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], :client_options => {:ssl => {:verify => false}}
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], :client_options => {:ssl => {:verify => false}},
+          :scope => 'email,friends_birthday,friends_relationship_status,friends_statuses'
 end
