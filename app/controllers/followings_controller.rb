8290fe19788860a908b776fa3d0429e9acc4bc42
Christina Chang <christina.chang@flatironschool.com>
2013-05-04 15:48:27 -0400

    add code to create new followings

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -1,2 +1,19 @@
 class FollowingsController < ApplicationController
+
+  def new
+    @following = Following.new
+  end
+
+  def create
+    params[:friend_id].each do |key, value|
+      if value == "1"
+        @following = Following.new
+        @following.user = current_user
+        @following.friend = Friend.find_by_id("#{key}")
+        @following.save
+      end
+    end
+    redirect_to followings_path
+  end
+
 end
